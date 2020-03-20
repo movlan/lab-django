47c12c9a297a7237bf223ff40364c9222587bbc0
Bahram Movlanov <bmovlanov@gmail.com>
2020-03-19 20:54:44 -0600

    initial

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('dog/create', views.DogCreate.as_view(), name='dog_create'),
     path('dog/<int:pk>/update', views.DogUpdate.as_view(), name='dogs_update'),
     path('dog/<int:pk>/delete', views.DogDelete.as_view(), name='dogs_delete'),
+    path('dog/<int:dog_id>/add_feeding', views.add_feeding, name='add_feeding'),
+    path('dog/<int:dog_id>/toy_assoc/<int:toy_id>', views.toy_assoc, name='toy_assoc')
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import request
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from .models import Dog, Toy
+from .forms import FeedingForm
 
 
 class DogCreate(CreateView):
@@ -22,5 +23,18 @@ def home(request):
 
 def dog(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
-    return render(request, 'dog.html', {'dog': dog})
+    toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
+    return render( request, 'dog.html', {'dog': dog, 'feeding_form': feeding_form, 'toys': toys_dog_doesnt_have })
 
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+
+def toy_assoc(request, dog_id, toy_id):
+    Dog.objects.get(id=dog_id).toys.add(toy_id)
+    return redirect('detail', dog_id=dog_id)
